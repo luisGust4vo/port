0330c734162eaa5781710252727477e44d9dd520
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2021-11-01 19:59:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function aviso(){
       })
       .then((willDelete) => {
         if (willDelete) {
-          swal("Vlww!", {
+          swal( {
              title: "Vlw!!!",
              icon: "success" 
           });
